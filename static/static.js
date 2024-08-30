2e143cc71d170c499188ea9e8afb0465199ad173
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2024-08-30 06:22:15 +0000

    edits: footer, and more

---
+++ static/static.js
@@ -0,0 +1,4 @@
+// JavaScript to toggle mobile menu visibility
+document.getElementById('mobileMenuToggle').addEventListener('click', function () {
+    document.getElementById('mobileMenu').classList.toggle('hidden');
+});
